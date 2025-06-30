dffd665a79bb7d6bcf1f077eaeb4ef3668e1ecce
Shinji Mitaka <3taka-0418@gmail.com>
2025-06-30 15:10:13 +0900

    add gradle project2

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
-    @Test
-    public void testSum() {
-        Calculate calculate = new Calculate();
-        int expected = 5;
-        assertEquals(expected, calculate.sum(2, 3));
-    }
+  @Test
+  public void testSum() {
+    Calculate calculate = new Calculate();
+    int expected = 5;
+    assertEquals(expected, calculate.sum(2, 3));
+  }
 }
